7b2237fcbf24cd12f00f3ac237bdfedad0ac4e85
Gabriel <gnvivanco1@espe.edu.ec>
2025-02-28 16:57:10 -0500

    Taller

--- PrimeraAppNetCore/CapaEntidad/SucursalCLS.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CapaEntid{
-    public class SucursalCLS
-    {
-        public int idSucursal { get; set; }
-        public string nombre { get; set; }
-        public string direccion { get; set; }
-    }
-}
--- PrimeraAppNetCore/PrimeraAppNetCore/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace PrimeraAppNetCore.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
